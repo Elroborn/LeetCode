0ff5f3e049d1047eb1ec5453affa2017368735f4
coderwangson <572043wyh@outlook.com>
2018-12-25 16:25:50 +0800

    Create 正则表达式匹配2.py

---
+++ 10_正则表达式匹配/正则表达式匹配2.py
@@ -0,0 +1,19 @@
+'''
+Created on 2018年12月21日
+
+@author: coderwangson
+'''
+"#codeing=utf-8"
+# ref ：https://blog.csdn.net/hk2291976/article/details/51165010
+class Solution:
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        
+             
+print(Solution().isMatch("", "a"))
+
+
