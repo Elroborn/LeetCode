640eaa60eeaf4e9b532be33c595d0c2acd2d9dc1
coderwangson <572043wyh@outlook.com>
2018-12-26 12:00:02 +0800

    Update 正则表达式匹配2.py

--- 10_正则表达式匹配/正则表达式匹配2.py
+++ 10_正则表达式匹配/正则表达式匹配2.py
@@ -4,7 +4,7 @@ Created on 2018年12月21日
 @author: coderwangson
 '''
 "#codeing=utf-8"
-# ref ：https://blog.csdn.net/hk2291976/article/details/51165010
+# ref ：https://blog.csdn.net/weixin_39781462/article/details/82999610
 class Solution:
     def isMatch(self, s, p):
         """
@@ -12,8 +12,29 @@ class Solution:
         :type p: str
         :rtype: bool
         """
+        n = len(s)
+        m = len(p)
+        #############################
+        ##########python二维数组坑！！！！！！！！！！！！！！！！############
+        ############################
+#         dp = [[False] *(m+1)]*(n+1)
+#         print(dp)
+#         dp[0][1] = True
+#         print(dp)
+        dp=[[False for j in range(len(p)+1)] for i in range(len(s)+1)]
+        dp[0][0] =True
+        for j in range(1,m+1):
+            dp[0][j] = j>=2 and p[j-1]=="*" and dp[0][j-2] ==True
         
-             
-print(Solution().isMatch("", "a"))
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if p[j-1]=="*":
+                    dp[i][j] = dp[i][j-2] or (dp[i-1][j] and (s[i-1]==p[j-2] or p[j-2]==".")) 
+         
+                else:
+                    dp[i][j] = (p[j-1]=="." or s[i-1]==p[j-1]) and dp[i-1][j-1]
+        return dp[n][m]
+print(Solution().isMatch("aa", "a*"))
+
 
 
