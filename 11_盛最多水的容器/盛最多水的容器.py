7dfef28cc1777d0faa909d2631f2fd2acb73fb30
coderwangson <572043wyh@outlook.com>
2019-02-02 16:33:07 +0800

    add

--- 11_盛最多水的容器/盛最多水的容器.py
+++ 11_盛最多水的容器/盛最多水的容器.py
@@ -4,3 +4,20 @@ Created on 2019/2/2 9:38
 @author: coderwangson
 """
 "#codeing=utf-8"
+
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        area = 0
+        for i in range(len(height)):
+            for j in range(i+1,len(height)):
+                width = j-i
+                high = min(height[i],height[j])
+                area = width * high if (width*high)>area else area
+        return area
+
+print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
+
--- 11_盛最多水的容器/盛最多水的容器2.py
+++ 11_盛最多水的容器/盛最多水的容器2.py
@@ -4,7 +4,7 @@ Created on 2019/2/2 9:38
 @author: coderwangson
 """
 "#codeing=utf-8"
-
+# ref https://www.cnblogs.com/wmx24/p/9043447.html
 class Solution:
     def maxArea(self, height):
         """
@@ -12,11 +12,17 @@ class Solution:
         :rtype: int
         """
         area = 0
-        for i in range(len(height)):
-            for j in range(i+1,len(height)):
-                width = j-i
-                high = min(height[i],height[j])
-                area = width * high if (width*high)>area else area
+        i = 0
+        j = len(height)-1
+        while i<j:
+            width = j-i
+            high = min(height[i],height[j])
+            newArea = width * high
+            area = newArea if newArea>area else area
+            if height[i]>height[j]:
+                j-=1
+            else:
+                i+=1
         return area
 
 print(Solution().maxArea([1,8,6,2,5,4,8,3,7]))
