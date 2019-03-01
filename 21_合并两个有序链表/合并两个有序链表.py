658cde545fedb38c8ea5cd1fcd438c43cb1b18c6
coderwangson <572043wyh@outlook.com>
2019-03-01 19:56:47 +0800

    Create 合并两个有序链表.py

---
+++ 21_合并两个有序链表/合并两个有序链表.py
@@ -0,0 +1,26 @@
+"""
+Created on 2019/3/1 19:53
+@File:合并两个有序链表.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+l1 = ListNode(1)
+import numpy as np
+n = np.random.randint(1,100,[4])
+for i in n:
+    print(i)
