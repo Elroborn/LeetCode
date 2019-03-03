b16aa9d18e3d5f70edc54533d7dd8d793087f35e
coderwangson <572043wyh@outlook.com>
2019-03-03 12:12:23 +0800

    Update 括号生成.py

--- 22_括号生成/括号生成.py
+++ 22_括号生成/括号生成.py
@@ -2,3 +2,26 @@
 Created on 2019/3/2 20:26
 @author: coderwangson
 """
+#ref https://blog.csdn.net/w8253497062015/article/details/80001249
+
+class Solution(object):
+    def dfs(self,cur,left,right,res):
+        if left==0 and right ==0:
+            cur+=""
+            res.append(cur)
+            return
+        else:
+            if left>0:
+                self.dfs(cur+"(",left-1,right,res)
+            if left<right:
+                self.dfs(cur+")",left,right-1,res)
+
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res =[]
+        self.dfs("",n,n,res)
+        return res
+print(Solution().generateParenthesis(1))
