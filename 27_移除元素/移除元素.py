b321eec9b09905de426721234e760aa1cc8663c7
coderwangson <572043wyh@outlook.com>
2019-03-10 13:58:36 +0800

    Update 移除元素.py

--- 27_移除元素/移除元素.py
+++ 27_移除元素/移除元素.py
@@ -4,3 +4,24 @@ Created on 2019/3/10 13:55
 @author: coderwangson
 """
 "#codeing=utf-8"
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = -1
+        j = 0
+        while j <len(nums):
+            if nums[j] != val:
+                i+=1
+                nums[i] = nums[j]
+            j+=1
+        return i+1
+
+print(Solution().removeElement( [3,2,2,3],3))
+
+
