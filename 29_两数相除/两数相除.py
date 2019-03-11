27434ed7edc03168ffcdfb30042536e993bba3e7
coderwangson <572043wyh@outlook.com>
2019-03-11 20:25:43 +0800

    Update 两数相除.py

--- 29_两数相除/两数相除.py
+++ 29_两数相除/两数相除.py
@@ -6,7 +6,6 @@ Created on 2019/3/11 19:41
 "#codeing=utf-8"
 
 # ref https://zhuanlan.zhihu.com/p/47708379
-import math
 class Solution(object):
     def divide(self, dividend, divisor):
         """
@@ -14,19 +13,26 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        # 除法转为指数和对数减法
-        if dividend ==0:
-            return 0
-
+        # 转为移位运算
         a = abs(dividend)
         b = abs(divisor)
-        res = math.exp(math.log(a)-math.log(b))
+        res = 0
+        # 如果a<b 则必定为0
+        while a>=b:
+            tmp = b
+            cur = 1
+            while a>(tmp<<1):
+                tmp = tmp<<1
+                cur=cur<<1
+            a-=tmp
+            res+=cur
+
         # ^ 异或 两个异号的话，两个条件肯定一个为假，一个为真 则异或为真
         if (divisor>0) ^ (dividend>0): res = -res
         if res >2**31-1: res = 2**31-1
         if res<-2**31: res = -2**31
-        return int(res)
+        return res
 
-print(Solution().divide(-2147483648,1))
+print(Solution().divide(10,3))
 
 
