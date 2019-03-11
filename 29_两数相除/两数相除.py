8f3461e8747a7ec259b7e615703007b8fae3e99f
coderwangson <572043wyh@outlook.com>
2019-03-11 19:42:35 +0800

    Create 两数相除.py

---
+++ 29_两数相除/两数相除.py
@@ -0,0 +1,16 @@
+"""
+Created on 2019/3/11 19:41
+@File:两数相除.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+# ref https://zhuanlan.zhihu.com/p/47708379
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        # 除法转为指数和对数减法
