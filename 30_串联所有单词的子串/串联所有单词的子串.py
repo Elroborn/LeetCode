278ce6d1b4d26303318bc1a0c08eac2c87c354a2
coderwangson <572043wyh@outlook.com>
2019-03-12 20:49:02 +0800

    Create 串联所有单词的子串.py

---
+++ 30_串联所有单词的子串/串联所有单词的子串.py
@@ -0,0 +1,44 @@
+"""
+Created on 2019/3/12 20:35
+@File:串联所有单词的子串.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+class Solution(object):
+    def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        n = len(words)
+        m = len(words[0])
+        map1 ={}
+        res = []
+        for i in words: map1[i]=1
+        # 从每个可能的起点遍历
+        for i in range(len(s)-m*n+1):
+            map2 ={}
+            for j in range(n):
+                # 从字符串里面从起点依次取子串 看是否在字典map1中
+                tmp = s[i+j*m:i+j*m+m]
+                # 如果没有，则直接跳过，因为匹配不到的
+                if not map1.has_key(tmp):break
+                # 如果有，则先把用过的暂存到map2中，并且标记为1，这样下次再次使用的时候进行比较，如果>1，则说明使用了两次
+                # 则跳过
+                map2[tmp] =1 if not map2.has_key(tmp) else map2[tmp]+1
+                # 说明使用了两次，则跳出
+                if map2[tmp] >map1[tmp]: break
+            # 说明没有break过
+            else:
+                res.append(i)
+        return res
+
+
+
+
+
+
+print(Solution().findSubstring("barfoothefoobarman",["foo","bar"]))
