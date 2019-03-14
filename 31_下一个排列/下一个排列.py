aa353e5a9fda1205c7ce2d0596c994887b53b5e7
coderwangson <572043wyh@outlook.com>
2019-03-14 20:44:55 +0800

    Update 下一个排列.py

--- 31_下一个排列/下一个排列.py
+++ 31_下一个排列/下一个排列.py
@@ -14,8 +14,6 @@ class Solution(object):
         # 如果用直接用i作为基准，那么需要判断i-1是否越过0，所以用i_1作为基准--
         # len(nums)-1 是最后一个下标
         i_1 = len(nums)-1 -1 # i_1 代表i-1
-        if i_1 <0: # 因为如果就一个元素，下面反转时候切片会出问题，所以直接做个判断返回
-            return None
         while i_1>= 0 and nums[i_1]>=nums[i_1+1]:
             i_1 -=1
         # 找到一个刚好比i_1大的值
@@ -24,10 +22,17 @@ class Solution(object):
             while j>=0 and nums[j]<=nums[i_1]:
                 j-=1
             nums[i_1],nums[j] = nums[j],nums[i_1]
-        # 从i开始反转一下（变得小了） 这样才是下一个
+        # 从i开始反转一下（变得为最小了） 这样才是下一个
         i = i_1 + 1
-        nums[i:] =nums[:i-1:-1]
-print(Solution().nextPermutation([1,2]))
+        self.reverse(nums,i,len(nums)-1)
+        return None
+    def reverse(self,nums,i,j):
+        while i<j:
+            nums[i],nums[j] = nums[j],nums[i]
+            i+=1
+            j-=1
+
+print(Solution().nextPermutation([3,2,1]))
 
 
 
