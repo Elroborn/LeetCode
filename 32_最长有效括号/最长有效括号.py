e19886f52a3fbc1e5e390e90e396d98c1e7db056
coderwangson <572043wyh@outlook.com>
2019-03-16 09:27:34 +0800

    Create 最长有效括号.py

---
+++ 32_最长有效括号/最长有效括号.py
@@ -0,0 +1,25 @@
+"""
+Created on 2019/3/16 9:20
+@File:最长有效括号.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # 如果第一个是 ( 的
+        for i in range(len(s)):
+            if s[i] =="(":
+                break
+        j = 0
+        for k in range(i,len(s)):
+            j = j +1 if s[k]=="(" else j-1
+            if j<0:break
+        return (k-i+1)//2
+
+print(Solution().longestValidParentheses(")()())"))
