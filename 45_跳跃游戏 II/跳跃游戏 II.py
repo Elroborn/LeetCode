98283e38c68dadb62d8f3292fd727a70bbb04bee
coderwangson <572043wyh@outlook.com>
2019-04-07 21:23:43 +0800

    Create 跳跃游戏 II.py

---
+++ 45_跳跃游戏 II/跳跃游戏 II.py
@@ -0,0 +1,14 @@
+"""
+Created on 2019/4/7 20:35
+@File:跳跃游戏 II.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+# may ref http://www.cnblogs.com/grandyang/p/4373533.html
+class Solution(object):
+    def jump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
