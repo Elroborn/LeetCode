a291f2eb03b58ce660a8627526dd23fa3d61d7f8
coderwangson <572043wyh@outlook.com>
2019-04-08 21:22:28 +0800

    Update 跳跃游戏 II.py

--- 45_跳跃游戏 II/跳跃游戏 II.py
+++ 45_跳跃游戏 II/跳跃游戏 II.py
@@ -6,9 +6,37 @@ Created on 2019/4/7 20:35
 "#codeing=utf-8"
 
 # may ref http://www.cnblogs.com/grandyang/p/4373533.html
+# bfs 超时
 class Solution(object):
     def jump(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        #先尝试用BFS做用list做队列
+        q = []
+        q.append(0)
+        n = len(nums)
+        res = 0
+        # 标记每层的起始结点位置和最后结点位置
+        firstNode = 0
+        lastNode = 1
+        while len(q)>0:
+            # 弹出一个
+            v = q.pop(0)
+            firstNode+=1
+            if v == n-1:
+                return res
+            if nums[v]>0:
+                next_level = list(range(v+1,v+1+nums[v]))
+                q.extend(next_level)
+            if firstNode == lastNode:
+                res +=1
+                firstNode = 0
+                lastNode = len(q)
+        return -1
+
+print(Solution().jump([2,3,1,1,4]))
+
+
+
