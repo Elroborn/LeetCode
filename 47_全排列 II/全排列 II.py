099f45a4d34e868ef637a8898cec45dedc8135ac
coderwangson <572043wyh@outlook.com>
2019-04-15 21:38:34 +0800

    Update 全排列 II.py

--- 47_全排列 II/全排列 II.py
+++ 47_全排列 II/全排列 II.py
@@ -4,35 +4,7 @@ Created on 2019/4/10 20:34
 @author: coderwangson
 """
 "#codeing=utf-8"
-import copy
 
-# 无[2, 1, 2, 1]
-# class Solution(object):
-#     def perm(self,nums,i,tmp,res,book):
-#         if i==len(nums):
-#             res.append(copy.copy(tmp))
-#             return
-#         for j in range(len(nums)):
-#             if book[j]!=0:continue
-#             book[j] =1
-#             tmp.append(nums[j])
-#             self.perm(nums,i+1,tmp,res,book)
-#             tmp.pop(nums[j])
-#             book[j] = 0
-#
-#     def permuteUnique(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: List[List[int]]
-#         """
-#         res = []
-#         tmp = []
-#         book = [0 for i in nums]
-#         self.perm(nums, 0, tmp,res,book)
-#         # res = [tuple(x) for x in res]
-#         # res = list(set(res))
-#         # res = [list(x) for x in res]
-#         return res
 import copy
 class Solution(object):
     def perm(self,a,start,res):
