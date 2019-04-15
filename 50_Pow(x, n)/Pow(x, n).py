8771b250603f19cafc111489242aa9f42e794269
coderwangson <572043wyh@outlook.com>
2019-04-15 21:38:42 +0800

    Update Pow(x, n).py

--- 50_Pow(x, n)/Pow(x, n).py
+++ 50_Pow(x, n)/Pow(x, n).py
@@ -5,7 +5,7 @@ Created on 2019/4/13 20:01
 """
 "#codeing=utf-8"
 
-
+# ref http://www.cnblogs.com/grandyang/p/4383775.html
 class Solution(object):
     def myPow(self, x, n):
         """
@@ -14,8 +14,11 @@ class Solution(object):
         :rtype: float
         """
         if n ==0:return 1
-        half = self.myPow(x,n//2)
-        if n%2 == 0:return half*half # 比如 2^4 = 2^2 * 2^2
-        else: return half*half*x # 比如2^5 = 2^2*2^2 *2
+        half = self.myPow(x,abs(n)//2)
+        res = 1
+        if n%2 == 0:res = half*half # 比如 2^4 = 2^2 * 2^2
+        else: res = half*half*x # 比如2^5 = 2^2*2^2 *2
+        # 因为在python中 -1//2 = -1
+        return res if n>0 else 1/res
 
-print(Solution().myPow(3,2))
+print(Solution().myPow(2,-1))
