8cbc7d11d63156efa90689f42ef4d4b77d5004f6
coderwangson <572043wyh@outlook.com>
2019-04-17 21:46:53 +0800

    Create 螺旋矩阵.py

---
+++ 54_螺旋矩阵/螺旋矩阵.py
@@ -0,0 +1,14 @@
+"""
+Created on 2019/4/17 21:42
+@File:螺旋矩阵.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
