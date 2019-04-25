b1f9b8291ba6a62c33f305767082ebef147aaee3
coderwangson <572043wyh@outlook.com>
2019-04-25 21:28:42 +0800

    Update 合并区间_1.py

--- 56_合并区间/合并区间_1.py
+++ 56_合并区间/合并区间_1.py
@@ -9,8 +9,8 @@ Created on 2019/4/19 20:31
 # Definition for an interval.
 class Interval(object):
     def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+        self[0] = s
+        self[1] = e
 
 class Solution(object):
 
@@ -21,12 +21,12 @@ class Solution(object):
         """
         if intervals ==[]:return []
         # 先排序
-        intervals.sort(key = lambda p:p.start)
+        intervals.sort(key = lambda p:p[0])
         res = []
         tmp = intervals[0]
         for i in intervals:
-            if tmp.end>=i.start:
-                tmp.end = max(i.end,tmp.end)
+            if tmp[1]>=i[0]:
+                tmp[1] = max(i[1],tmp[1])
             else:
                 res.append(tmp)
                 tmp = i
@@ -34,7 +34,7 @@ class Solution(object):
         return res
 def show(l):
     for i in l:
-        print(i.start,'-',i.end)
+        print(i[0],'-',i[1])
 l = []
 s = Interval(1,4)
 l.append(s)
