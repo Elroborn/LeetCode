d26e9386b1ab30581840fac7deb3f5f59aa27945
coderwangson <572043wyh@outlook.com>
2019-04-28 17:38:40 +0800

    Update 第k个排列.py

--- 60_第k个排列/第k个排列.py
+++ 60_第k个排列/第k个排列.py
@@ -9,17 +9,17 @@ import copy
 class Solution(object):
     def dfs(self,data,i,tmp,count,k,res):
         if i==len(data):
-            count+=1
-            if count ==k:
-                res.append(copy.copy(tmp))
+            res.append(copy.copy(tmp))
+            return
+
         for j in range(i,len(data)):
-            tmp.append(data[j])
+            tmp.append(str(data[j]))
             data[i],data[j] = data[j],data[i]
             self.dfs(data,i+1,tmp,count,k,res)
             data[i], data[j] = data[j], data[i]
-            tmp.remove(data[j])
-
-
+            tmp.pop(-1)
+        if len(res) ==k:
+            return
     def getPermutation(self, n, k):
         """
         :type n: int
@@ -30,6 +30,6 @@ class Solution(object):
         res = []
         data = list(range(1,n+1))
         self.dfs(data,0,tmp,0,k,res)
-        return res
-print(Solution().getPermutation(4,3))
+        return ''.join(res[k-1])
+print(Solution().getPermutation(3,3))
 
