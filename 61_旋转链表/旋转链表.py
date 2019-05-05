0987f0a5aa3d98cbe5083e888b31137f8fdf4be4
coderwangson <572043wyh@outlook.com>
2019-05-05 17:32:44 +0800

    Create 旋转链表.py

---
+++ 61_旋转链表/旋转链表.py
@@ -0,0 +1,21 @@
+"""
+Created on 2019/5/5 17:27
+@File:旋转链表.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
