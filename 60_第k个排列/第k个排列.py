83b1f477a6fc43d4fdad6a41129ad02b38b4b7b7
coderwangson <572043wyh@outlook.com>
2019-05-05 17:32:41 +0800

    Update 第k个排列.py

--- 60_第k个排列/第k个排列.py
+++ 60_第k个排列/第k个排列.py
@@ -5,28 +5,33 @@ Created on 2019/4/27 21:14
 """
 "#codeing=utf-8"
 import copy
-
+# https://segmentfault.com/a/1190000018246661
 class Solution(object):
-    def dfs(self,data,i,count,k,res):
-        if i==len(data):
-            res.append(copy.copy(data))
-            return
 
-        for j in range(i,len(data)):
-            data[i],data[j] = data[j],data[i]
-            self.dfs(data,i+1,count,k,res)
-            data[i], data[j] = data[j], data[i]
     def getPermutation(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: str
         """
-        tmp = []
-        res = []
-        data = list(range(1,n+1))
-        self.dfs(data,0,0,k,res)
-        print(res)
-        return None
-print(Solution().getPermutation(3,3))
+        nums = [str(i) for i in range(1,n+1)]
+        if k==1:
+            return "".join(nums)
+        fact = 1
+        for i in range(1,n):
+            fact *=i # (n-1) !
+        round = n - 1 # 轮数
+        k -= 1 # 因为数据是从1开始，索引是从0开始，为了统一 -=1
+        finalRes = []
+        while round >= 0:
+            index = int(k / fact)
+            k %= fact
+            finalRes.append(nums[index])
+            nums.remove(nums[index])
+            if round > 0:
+                fact /= round
+            round -= 1
+        return "".join(finalRes)
+
+print(Solution().getPermutation(4,2))
 
