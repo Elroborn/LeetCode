dede3d899fea4e6d6c00e75985c2e9aa937511c0
coderwangson <572043wyh@outlook.com>
2019-05-08 17:21:16 +0800

    Create 不同路径.py

---
+++ 62_不同路径/不同路径.py
@@ -0,0 +1,15 @@
+"""
+Created on 2019/5/8 17:11
+@File:不同路径.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
