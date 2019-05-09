eb6eaa8c8ca3ead311bd458a304165f92e47fe1e
coderwangson <572043wyh@outlook.com>
2019-05-09 17:18:22 +0800

    Update 不同路径.py

--- 62_不同路径/不同路径.py
+++ 62_不同路径/不同路径.py
@@ -4,7 +4,7 @@ Created on 2019/5/8 17:11
 @author: coderwangson
 """
 "#codeing=utf-8"
-
+# https://juejin.im/post/5b927e795188255c6c621e11
 
 class Solution(object):
     def uniquePaths(self, m, n):
@@ -13,3 +13,12 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        dp = [[1]*m for i in range(n)]
+        for i in range(1,n):
+            for j in range(1,m):
+                print(i,j)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[n-1][m-1]
+
+print(Solution().uniquePaths(7,3))
