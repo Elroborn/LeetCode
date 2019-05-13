42c0a35480e3817c34b443ac5af903576835d1bc
coderwangson <572043wyh@outlook.com>
2019-05-13 21:39:19 +0800

    Update 不同路径 II.py

--- 63_不同路径 II/不同路径 II.py
+++ 63_不同路径 II/不同路径 II.py
@@ -14,13 +14,18 @@ class Solution(object):
         """
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
-        dp = [[1]*n for i in range(m)] # TODO 缺少一个判断这个不一定刚开始就都初始化1 比如[[1]]第一个就堵了，则为0 
-        for i in range(1,m):
-            for j in range(1,n):
+        if m==0 or obstacleGrid[0][0]==1: # 没有路，或者出门就堵了
+            return 0
+        dp = [[1 for j in range(n)] for i in range(m)] #
+        # 从0,0开始发散
+        for i in range(m):
+            for j in range(n):
                 if obstacleGrid[i][j] == 1: dp[i][j] =0 # 如果障碍，则不能通过，所以置为0
+                elif j==0: dp[i][j] = dp[i-1][j] # j==0即为最左边的一列，所以和上面相关
+                elif i==0: dp[i][j] = dp[i][j-1]
                 else: dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
 
 print(Solution().uniquePathsWithObstacles(
 
-    [[0, 0], [1, 1], [0, 0]]))
+    [[1, 0]]))
