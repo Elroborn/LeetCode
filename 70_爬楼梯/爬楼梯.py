b6b3c251cce351292d84cb0b41fb255f0b54e8c7
coderwangson <572043wyh@outlook.com>
2019-05-29 20:36:49 +0800

    Create 爬楼梯.py

---
+++ 70_爬楼梯/爬楼梯.py
@@ -0,0 +1,21 @@
+"""
+Created on 2019/5/29 19:58
+@File:爬楼梯.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n ==1:return 1
+        if n==2:return 2
+        f1 = 1
+        f2 = 2
+        for i in range(3,n+1):
+            f = f1 + f2
+            f1,f2 = f2,f
+        return f
+print(Solution().climbStairs(5))
