2e80a79e55cba896341d191e3493fbe7aac25d27
coderwangson <572043wyh@outlook.com>
2019-06-18 15:46:10 +0800

    Create 组合.py

---
+++ 77_组合/组合.py
@@ -0,0 +1,13 @@
+"""
+Created on 2019/6/18 15:31
+@File:组合.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
