4ae3807fab06b076ec096374cf69327cd6d092ef
coderwangson <572043wyh@outlook.com>
2019-06-18 15:46:18 +0800

    Update 编辑距离.py

--- 72_编辑距离/编辑距离.py
+++ 72_编辑距离/编辑距离.py
@@ -25,7 +25,7 @@ class Solution(object):
         for i in range(1,rows+1):
             for j in range(1,cols+1):
                 if word1[i-1] == word2[j-1]:dp[i][j] = dp[i-1][j-1]
-                # 分三种情况取最小的 插入 dp[i][j-1] + 1 删除 dp[i-1][j] +1 替换 dp[i-1][j-1] + 1
+                # 分三种情况取最小的  插入 dp[i][j-1] + 1  删除dp[i-1][j] +1 替换 dp[i-1][j-1] + 1
                 else:dp[i][j] = min(dp[i][j-1] + 1,dp[i-1][j] +1,dp[i-1][j-1] + 1)
         return dp[i][j]
 
