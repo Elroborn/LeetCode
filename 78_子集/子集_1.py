0f826a50eb3d7c9f4e8261a14d81b96087006416
coderwangson <572043wyh@outlook.com>
2019-07-09 08:38:53 +0800

    Create 子集_1.py

---
+++ 78_子集/子集_1.py
@@ -0,0 +1,26 @@
+"""
+Created on 2019/6/25 19:15
+@File:子集.py
+@author: coderwangson
+"""
+"#codeing=utf-8"
+# 二进制法
+# https://leetcode-cn.com/problems/subsets/solution/er-jin-zhi-wei-zhu-ge-mei-ju-dfssan-chong-si-lu-9c/
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # 刚好幂集是2^n 所以可以用二进制代表比如 0100代表第1个位置上面取
+        res  = []
+        for i in range(0,1<<len(nums)):# i相当于从 0000 - 1111
+            tmp = []
+            for j in range(0,len(nums)): # 现在有了i，每次遍历到j，只需要看i对应二进制在j位置是否为1
+                if (i>>(len(nums)-1 - j) & 1) ==1: # 用了小技巧，比如当前i为0100 遍历到的j = 1，那么i>>nums-1 - j为 0001 所以&1为1
+                                            # 其实就是把j位置上的数字右移到最后，这样可以&1看是否为1
+                    tmp.append(nums[j])
+            res.append(tmp)
+        return res
+
+print(Solution().subsets([1,2,3]))
